c452ab8e11723e16baadb4a7add38eb4fb134fdd
Taylor Kelley <taylorgkelley@outlook.com>
2025-08-10 16:33:36 -0500

    fix: fixed syntax errors from previous changes

--- src/interfaces/resolvers/category.resolver.ts
+++ src/interfaces/resolvers/category.resolver.ts
@@ -1,32 +1,31 @@
 import { Categories } from '@/application/useCases/Categories.useCase';
 import { type ContextType } from '@/infrastructure/apollo/standalone';
 import { type Resolvers } from '@/types/schema';
-import { UUID } from 'node:crypto';
 
 const categoryResolver: Resolvers<ContextType> = {
-  Query: {
-    categories: async (_parent, _args, { req }, _info) => {
-      return await Categories.forUser(req.user).getAll();
-    },
-    categoryById: async (_parent, { id }, { req }, _info) => {
-      const category = await Categories.forUser(req.user).get(id as UUID)!;
+	Query: {
+		categories: async (_parent, { monthDate }, { req }, _info) => {
+			return await Categories.forUser(req.user).getAll({ monthDate });
+		},
+		categoryById: async (_parent, { id }, { req }, _info) => {
+			const category = await Categories.forUser(req.user).get({ id })!;
 
-      if (!category) throw new Error('Cannot find a category with that Id');
+			if (!category) throw new Error('Cannot find a category with that Id');
 
-      return category;
-    },
-  },
-  Mutation: {
-    createCategory: async (_parent, { input }, { req }, _info) => {
-      return await Categories.forUser(req.user).create(input);
-    },
-    updateCategory: async (_parent, { input }, { req }, _info) => {
-      return Categories.forUser(req.user).update(input);
-    },
-    deleteCategory: async (_parent, { id }, { req }, _info) => {
-      return Categories.forUser(req.user).delete(id as UUID);
-    },
-  },
+			return category;
+		},
+	},
+	Mutation: {
+		createCategory: async (_parent, { input }, { req }, _info) => {
+			return await Categories.forUser(req.user).create(input);
+		},
+		updateCategory: async (_parent, { input }, { req }, _info) => {
+			return Categories.forUser(req.user).update(input);
+		},
+		deleteCategory: async (_parent, { id }, { req }, _info) => {
+			return Categories.forUser(req.user).delete({ id });
+		},
+	},
 };
 
 export default categoryResolver;
