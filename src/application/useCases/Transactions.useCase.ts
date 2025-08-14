67578f3b1ac7c94b9f1b7a7ccc9b052ad14fd9fc
Taylor Kelley <taylorgkelley@outlook.com>
2025-08-13 19:28:44 -0500

    fix: Fixed issue with monthDate condition's for Category and Transaction

--- src/application/useCases/Transactions.useCase.ts
+++ src/application/useCases/Transactions.useCase.ts
@@ -1,132 +1,133 @@
 import { db } from '@/infrastructure/database';
 import { transactionTable } from '@/infrastructure/database/schema';
 import {
-  type QueryTransactionsArgs,
-  type QueryTransactionByIdArgs,
-  type MutationCreateTransactionArgs,
-  type MutationUpdateTransactionArgs,
-  type Transaction,
-  MutationDeleteTransactionArgs,
+	type QueryTransactionsArgs,
+	type QueryTransactionByIdArgs,
+	type MutationCreateTransactionArgs,
+	type MutationUpdateTransactionArgs,
+	type Transaction,
+	MutationDeleteTransactionArgs,
 } from '@/types/schema';
 import { User } from '@/types/User';
-import { and, eq, gt, lte } from 'drizzle-orm';
+import { and, asc, eq, gte, lt } from 'drizzle-orm';
 import { type UUID } from 'node:crypto';
 
 export class Transactions {
-  public static forUser(user: User | null) {
-    if (user === null) throw new Error('Unauthorized');
+	public static forUser(user: User | null) {
+		if (user === null) throw new Error('Unauthorized');
 
-    return new Transactions(user.id);
-  }
+		return new Transactions(user.id);
+	}
 
-  private _userId: UUID;
-  public constructor(userId: UUID) {
-    this._userId = userId;
-  }
+	private _userId: UUID;
+	public constructor(userId: UUID) {
+		this._userId = userId;
+	}
 
-  public async getAll({
-    monthDate,
-    pagination,
-  }: QueryTransactionsArgs): Promise<Transaction[]> {
-    const query = db
-      .select()
-      .from(transactionTable)
-      .where(
-        and(
-          eq(transactionTable.userId, this._userId),
-          !monthDate
-            ? undefined
-            : lte(
-                transactionTable.date,
-                new Date(monthDate.getFullYear(), monthDate.getMonth(), 1)
-              ),
-          !monthDate
-            ? undefined
-            : gt(
-                transactionTable.date,
-                new Date(monthDate.getFullYear(), monthDate.getMonth(), 0)
-              )
-        )
-      );
+	public async getAll({
+		monthDate,
+		pagination,
+	}: QueryTransactionsArgs): Promise<Transaction[]> {
+		const query = db
+			.select()
+			.from(transactionTable)
+			.where(
+				and(
+					eq(transactionTable.userId, this._userId),
+					!monthDate
+						? undefined
+						: gte(
+								transactionTable.date,
+								new Date(monthDate.getFullYear(), monthDate.getMonth(), 1)
+						  ),
+					!monthDate
+						? undefined
+						: lt(
+								transactionTable.date,
+								new Date(monthDate.getFullYear(), monthDate.getMonth() + 1, 1)
+						  )
+				)
+			)
+			.orderBy(asc(transactionTable.date));
 
-    if (pagination?.limit) query.limit(pagination.limit);
-    if (pagination?.offset) query.offset(pagination.offset);
+		if (pagination?.limit) query.limit(pagination.limit);
+		if (pagination?.offset) query.offset(pagination.offset);
 
-    const result = (await query) as Transaction[];
+		const result = (await query) as Transaction[];
 
-    return result;
-  }
+		return result;
+	}
 
-  public async get({
-    id,
-  }: QueryTransactionByIdArgs): Promise<Transaction | undefined> {
-    const result = (
-      await db
-        .select()
-        .from(transactionTable)
-        .where(
-          and(
-            eq(transactionTable.userId, this._userId),
-            eq(transactionTable.id, id)
-          )
-        )
-    )?.[0] as Transaction;
+	public async get({
+		id,
+	}: QueryTransactionByIdArgs): Promise<Transaction | undefined> {
+		const result = (
+			await db
+				.select()
+				.from(transactionTable)
+				.where(
+					and(
+						eq(transactionTable.userId, this._userId),
+						eq(transactionTable.id, id)
+					)
+				)
+		)?.[0] as Transaction;
 
-    return result;
-  }
+		return result;
+	}
 
-  public async create({
-    input,
-  }: MutationCreateTransactionArgs): Promise<Transaction> {
-    const result = (
-      await db
-        .insert(transactionTable)
-        .values({ ...input, userId: this._userId })
-        .returning()
-    )[0] as Transaction;
+	public async create({
+		input,
+	}: MutationCreateTransactionArgs): Promise<Transaction> {
+		const result = (
+			await db
+				.insert(transactionTable)
+				.values({ ...input, userId: this._userId })
+				.returning()
+		)[0] as Transaction;
 
-    return result;
-  }
+		return result;
+	}
 
-  public async update({
-    input,
-  }: MutationUpdateTransactionArgs): Promise<Transaction> {
-    const result = (
-      await db
-        .update(transactionTable)
-        .set({
-          categoryId: input.categoryId || undefined,
-          title: input.title || undefined,
-          amount: input.amount || undefined,
-          transactionType: input.transactionType || undefined,
-          date: input.date || undefined,
-          description: input.description || undefined,
-        })
-        .where(
-          and(
-            eq(transactionTable.userId, this._userId),
-            eq(transactionTable.id, input.id)
-          )
-        )
-        .returning()
-    )[0] as Transaction;
+	public async update({
+		input,
+	}: MutationUpdateTransactionArgs): Promise<Transaction> {
+		const result = (
+			await db
+				.update(transactionTable)
+				.set({
+					categoryId: input.categoryId || undefined,
+					title: input.title || undefined,
+					amount: input.amount || undefined,
+					transactionType: input.transactionType || undefined,
+					date: input.date || undefined,
+					description: input.description || undefined,
+				})
+				.where(
+					and(
+						eq(transactionTable.userId, this._userId),
+						eq(transactionTable.id, input.id)
+					)
+				)
+				.returning()
+		)[0] as Transaction;
 
-    return result;
-  }
+		return result;
+	}
 
-  public async delete({ id }: MutationDeleteTransactionArgs): Promise<UUID> {
-    const result = (
-      await db
-        .delete(transactionTable)
-        .where(
-          and(
-            eq(transactionTable.userId, this._userId),
-            eq(transactionTable.id, id)
-          )
-        )
-        .returning()
-    )[0];
+	public async delete({ id }: MutationDeleteTransactionArgs): Promise<UUID> {
+		const result = (
+			await db
+				.delete(transactionTable)
+				.where(
+					and(
+						eq(transactionTable.userId, this._userId),
+						eq(transactionTable.id, id)
+					)
+				)
+				.returning()
+		)[0];
 
-    return result.id as UUID;
-  }
+		return result.id as UUID;
+	}
 }
