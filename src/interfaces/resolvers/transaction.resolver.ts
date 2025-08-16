a61cd98a0569c9d589cf62ff65db01a84b96eaf9
Taylor Kelley <taylorgkelley@outlook.com>
2025-08-16 18:06:43 -0500

    style: changed transactionById to transaction query

--- src/interfaces/resolvers/transaction.resolver.ts
+++ src/interfaces/resolvers/transaction.resolver.ts
@@ -3,38 +3,38 @@ import { type ContextType } from '@/infrastructure/apollo/standalone';
 import { type Resolvers } from '@/types/schema';
 
 const transactionResolver: Resolvers<ContextType> = {
-  Query: {
-    transactions: async (
-      _parent,
-      { monthDate, pagination },
-      { req },
-      _info
-    ) => {
-      return await Transactions.forUser(req.user).getAll({
-        monthDate,
-        pagination,
-      });
-    },
-    transactionById: async (_parent, { id }, { req }, _info) => {
-      const transaction = await Transactions.forUser(req.user).get({ id })!;
+	Query: {
+		transactions: async (
+			_parent,
+			{ monthDate, pagination },
+			{ req },
+			_info
+		) => {
+			return await Transactions.forUser(req.user).getAll({
+				monthDate,
+				pagination,
+			});
+		},
+		transaction: async (_parent, { id }, { req }, _info) => {
+			const transaction = await Transactions.forUser(req.user).get({ id })!;
 
-      if (!transaction)
-        throw new Error('Cannot find a transaction with that Id');
+			if (!transaction)
+				throw new Error('Cannot find a transaction with that Id');
 
-      return transaction;
-    },
-  },
-  Mutation: {
-    createTransaction: async (_parent, { input }, { req }, _info) => {
-      return await Transactions.forUser(req.user).create({ input });
-    },
-    updateTransaction: async (_parent, { input }, { req }, _info) => {
-      return Transactions.forUser(req.user).update({ input });
-    },
-    deleteTransaction: async (_parent, { id }, { req }, _info) => {
-      return Transactions.forUser(req.user).delete({ id });
-    },
-  },
+			return transaction;
+		},
+	},
+	Mutation: {
+		createTransaction: async (_parent, { input }, { req }, _info) => {
+			return await Transactions.forUser(req.user).create({ input });
+		},
+		updateTransaction: async (_parent, { input }, { req }, _info) => {
+			return Transactions.forUser(req.user).update({ input });
+		},
+		deleteTransaction: async (_parent, { id }, { req }, _info) => {
+			return Transactions.forUser(req.user).delete({ id });
+		},
+	},
 };
 
 export default transactionResolver;
